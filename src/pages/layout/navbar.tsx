b33d727280395e78ca84e40c0eeab6e1c0f12c1d
Kade Illian <kade.illian@biproxi.com>
2023-06-18 00:50:08 -0600

    cleaned up sidebar

--- src/pages/layout/navbar.tsx
+++ src/pages/layout/navbar.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 export const Navbar: React.FC = () => {
   return (
     <>
-      <nav className="flex items-center justify-between flex-wrap bg-gradient-to-b from-[#D90429] to-[#EF233C] p-6">
+      <nav className="flex items-center justify-between flex-wrap bg-[#2B2D42] p-6">
         <Link
-          className="flex items-center flex-shrink-0 text-white mr-6"
+          className="flex items-center flex-shrink-0 text-white mr-6 hover:text-[#D90429]"
           href="/"
         >
           <Image
@@ -17,38 +17,13 @@ export const Navbar: React.FC = () => {
           />
           <span className="font-semibold text-2xl tracking-tight pl-4">Data Dive</span>
         </Link> 
-        <div className="block lg:hidden">
-          <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <title>Menu</title>
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M2 5h16v1H2V5zm0 4h16v1H2V9zm0 4h16v1H2v-1z"
-              />
-            </svg>
-          </button>
-        </div>
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div className="text-sm lg:flex-grow"></div>
           <div>
             <a
               href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-              Docs
-            </a>
-            <a
-              href="#responsive-header"
-              className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
-              Examples
-            </a>
-            <a
-              href="#responsive-header"
               className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white">
-              Blog
+              Login
             </a>
           </div>
         </div>
--- src/pages/layout/sidebar.tsx
+++ src/pages/layout/sidebar.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 const Sidebar: React.FC = () => {
   return (
     <>
-      <div className="flex flex-col w-64 h-screen px-4 py-8 bg-white border-r dark:bg-gray-800 dark:border-gray-600">
+      <div className="flex flex-col w-64 h-screen px-4 py-8 bg-[#EF233C] border-r">
         <h2 className="text-3xl font-semibold text-gray-800 dark:text-white">Data Dive</h2>
         <p className="text-sm text-gray-400 dark:text-gray-300">Welcome to Data Dive!  Browse some of our projects to see clever solutions to complex data problems.</p>
         <div className="flex flex-col mt-8">
