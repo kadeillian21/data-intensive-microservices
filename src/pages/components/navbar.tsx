87af8d01bee447ea6a35ce112d4c9aab47583223
Kade Illian <kade.illian@biproxi.com>
2023-06-19 21:41:26 -0600

    working sidebar, though the load times feel slow

--- src/pages/components/navbar.tsx
+++ src/pages/components/navbar.tsx
@@ -1,23 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export const Navbar: React.FC = () => {
   return (
     <>
       <nav className="flex items-center justify-between flex-wrap bg-[#003F88] p-6 ">
-        <Link
-          className="flex items-center flex-shrink-0 text-[#FFD500] mr-6 hover:text-white"
-          href="/"
-        >
-          <Image
-            src="/favicon-yellow.svg"
-            alt="Data Dive Logo"
-            width={50}
-            height={50}
-            className="fill"
-          />
-          <span className="font-semibold text-2xl tracking-tight pl-4">Data Dive</span>
-        </Link> 
         <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
           <div className="text-sm lg:flex-grow"></div>
           <div>
--- src/pages/components/sidebar.tsx
+++ src/pages/components/sidebar.tsx
@@ -8,14 +8,16 @@ import {
   GlobeAltIcon
 } from '@heroicons/react/24/outline'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 const navigation = [
-  { name: 'Home', href: '/', icon: HomeIcon, current: true },
-  { name: 'Data Dive Examples', href: '/data-dive', icon: DocumentDuplicateIcon, current: false },
-  { name: 'Code', href: '/code', icon: FolderIcon, current: false },
-  { name: 'About Us', href: '/about', icon: GlobeAltIcon, current: false },
-  { name: 'Blog', href: '/blog', icon: PencilSquareIcon, current: false },
-  { name: 'Contact Us', href: '/contact', icon: PhoneIcon, current: false },
+  { name: 'Home', href: '/', icon: HomeIcon },
+  { name: 'Data Dive Examples', href: '/data-dive', icon: DocumentDuplicateIcon },
+  { name: 'Code', href: '/code', icon: FolderIcon },
+  { name: 'About Us', href: '/about', icon: GlobeAltIcon },
+  { name: 'Blog', href: '/blog', icon: PencilSquareIcon },
+  { name: 'Contact Us', href: '/contact', icon: PhoneIcon },
 ]
 
 function classNames(...classes: string[]) {
@@ -23,51 +25,74 @@ function classNames(...classes: string[]) {
 }
 
 export const Sidebar: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<string>('/')
+  const [activeTabName, setActiveTabName] = useState<string>('Home')
+  const [activeTabIcon, setActiveTabIcon] = useState(HomeIcon as unknown)
+  
+  useEffect(() => {
+    setActiveTab(window.location.pathname)
+    if (window.location.pathname === '/') {
+      setActiveTabName('Home')
+      setActiveTabIcon(HomeIcon)
+    } else if (window.location.pathname === '/data-dive') {
+      setActiveTabName('Data Dive Examples')
+      setActiveTabIcon(DocumentDuplicateIcon)
+    } else if (window.location.pathname === '/code') {
+      setActiveTabName('Code')
+      setActiveTabIcon(FolderIcon)
+    } else if (window.location.pathname === '/about') {
+      setActiveTabName('About Us')
+      setActiveTabIcon(GlobeAltIcon)
+    } else if (window.location.pathname === '/blog') {
+      setActiveTabName('Blog')
+      setActiveTabIcon(PencilSquareIcon)
+    } else if (window.location.pathname === '/contact') {
+      setActiveTabName('Contact Us')
+      setActiveTabIcon(PhoneIcon)
+    }
+  }, [activeTab])
   return (
-    <div className="fixed top-[98px] left-0 h-screen w-64 flex flex-col gap-y-5 overflow-y-auto bg-[#FFD500] px-6">
+    <div className="fixed top-0 left-0 h-screen w-64 flex flex-col gap-y-5 overflow-y-auto bg-[#FFD500] px-6 pt-8">
+      <Link
+        className="flex items-center flex-shrink-0 text-[#003F88] mr-6 hover:text-white"
+        href="/"
+      >
+        <Image
+          src="/favicon-blue.svg"
+          alt="Data Dive Logo"
+          width={50}
+          height={50}
+          className="fill"
+        />
+        <span className="font-semibold text-2xl tracking-tight pl-4">Data Dive</span>
+      </Link> 
       <nav className="mt-4 flex flex-1 flex-col">
         <ul role="list" className="flex flex-1 flex-col gap-y-7">
           <li>
             <ul role="list" className="-mx-2 space-y-1">
-              {navigation.map((item) => (
-                <li key={item.name}>
+              {navigation.map((page) => (
+                <li key={page.name}>
                   <a
-                    href={item.href}
+                    href={page.href}
                     className={classNames(
-                      item.current
+                      activeTabName === page.name
                         ? 'bg-[#00296B] text-white'
                         : 'text-[#00296B] hover:text-white hover:bg-[#00296B]',
                       'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
                     )}
                   >
-                    <item.icon
+                    <page.icon
                       className={classNames(
-                        item.current ? 'text-white' : 'text-[#00296B] group-hover:text-white',
+                        activeTabIcon === page.icon ? 'text-white' : 'text-[#00296B] group-hover:text-white',
                         'h-6 w-6 shrink-0'
                       )}
                       aria-hidden="true"
                     />
-                    {item.name}
+                    {page.name}
                   </a>
                 </li>
               ))}
             </ul>
-          </li>
-          <li className="-mx-6 mt-auto">
-            <a
-              href="#"
-              className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-indigo-700"
-            >
-              <Image
-                className="h-8 w-8 rounded-full bg-indigo-700"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-                width={32}
-                height={32}
-              />
-              <span className="sr-only">Your profile</span>
-              <span aria-hidden="true">Tom Cook</span>
-            </a>
           </li>
         </ul>
       </nav>
